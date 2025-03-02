93b14458bd4cbe96ea5fdba381bfa494dc28386c
Astagina <72367928+MuhammadAqilAstagina@users.noreply.github.com>
2025-03-02 14:54:27 +0700

    Update index.js
    memperbaiki semua code js

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
-const questions = document.querySelectorAll(".question");
-const faqs = document.querySelectorAll(".faq");
-const actived = document.querySelectorAll(".faq-active");
+const plusIcon = document.querySelectorAll(".plus");
+const minIcon = document.querySelectorAll(".min");
+const acc = document.querySelectorAll("h3");
+const answear = document.querySelectorAll(".accordion__answer");
 
-questions.forEach((faq) => {
-  faq.addEventListener("click", () => {
-    faqs.forEach((faq) => faq.classList.remove("faq-active"));
-    const parentEl = box.parentElement;
-    parentEl.classList.add("faq-active");
+for (let i = 0; i < plusIcon.length; i++) {
+  plusIcon[i].addEventListener("click", function () {
+    plusIcon[i].classList.toggle("hide");
+    minIcon[i].classList.toggle("hide");
+    answear[i].classList.toggle("hide");
   });
-});
+}
+
+for (let i = 0; i < minIcon.length; i++) {
+  minIcon[i].addEventListener("click", function () {
+    plusIcon[i].classList.toggle("hide");
+    minIcon[i].classList.toggle("hide");
+    answear[i].classList.toggle("hide");
+  });
+}
+
+for (let i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function () {
+    answear[i].classList.toggle("hide");
+    plusIcon[i].classList.toggle("hide");
+    minIcon[i].classList.toggle("hide");
+  });
+}
